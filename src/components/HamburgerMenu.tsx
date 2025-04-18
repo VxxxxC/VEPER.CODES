18b027adae3aee69a51dbebd254e104586f3357b
vxxxxc <>
2025-04-19 00:02:12 +0800

    create works/index route

--- src/components/HamburgerMenu.tsx
+++ src/components/HamburgerMenu.tsx
@@ -4,7 +4,7 @@ import { Menu, Transition } from "@headlessui/react";
 import ThemeToggle from "./ThemeToggle";
 
 function HeaderComponent() {
-  const headerItems = ["Home", "Project", "Blog"];
+  const headerItems = ["Home", "Works", "Project", "Blog"];
 
   const [page, setPage]: any = useState("");
 
