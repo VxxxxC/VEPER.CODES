2d96fd61a2ac8b8396ce38335a2c0523af4146d1
vxxxxc <>
2025-04-24 00:05:29 +0800

    commit before merge

--- .astro/content.d.ts
+++ .astro/content.d.ts
@@ -147,10 +147,18 @@ declare module 'astro:content' {
 	};
 
 	type DataEntryMap = {
-		
+		"blogs": Record<string, {
+  id: string;
+  body?: string;
+  collection: "blogs";
+  data: any;
+  rendered?: RenderedContent;
+  filePath?: string;
+}>;
+
 	};
 
 	type AnyEntryMap = ContentEntryMap & DataEntryMap;
 
-	export type ContentConfig = typeof import("../src/content.config.mjs");
+	export type ContentConfig = typeof import("../src/content.config.js");
 }
