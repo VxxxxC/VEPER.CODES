b133f3ae2f8f5c98c6a98f45b91de6dd7845ea09
vxxxxc <>
2025-04-23 23:16:03 +0800

    change back button to history.back() instead of hardcode URL path

--- src/components/ReturnButton.tsx
+++ src/components/ReturnButton.tsx
@@ -3,13 +3,13 @@ import { IoArrowBack } from "react-icons/io5/index";
 
 function ReturnButton() {
   return (
-    <a href="/blog">
+    <button onClick={() => history.back()}>
       <IoArrowBack
         size={46}
         className="p-2 dark:hover:bg-white dark:hover:text-black hover:bg-black hover:text-white bg-opacity-30 rounded-xl"
         aria-label="return button"
       />
-    </a>
+    </button>
   );
 }
 
